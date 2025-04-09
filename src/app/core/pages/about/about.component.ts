f21215560720aba7926f1201a9463c1c8e1982d0
Ayush2395 <ak005355@gmail.com>
2025-04-10 00:03:49 +0530

    Fix some styling and add the tech stack to about section.

--- src/app/core/pages/about/about.component.ts
+++ src/app/core/pages/about/about.component.ts
@@ -2,10 +2,12 @@ import { Component, computed, inject } from '@angular/core';
 import { ResponsiveService } from '../../services/responsive.service';
 import { NgClass } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
-  imports: [NgClass, MatCardModule],
+  imports: [NgClass, MatCardModule, MatListModule, MatIconModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
@@ -21,20 +23,68 @@ export class AboutComponent {
 
   public experiences = [
     {
+      companyName: 'de facto Infotech',
+      responsibility:
+        '.NET Developer, responsible to handle the ASPNET Core Web API and web application build up with different frontend frameworks.',
+      designation: 'Fullstack developer - Fulltime',
+      techStack: [
+        {
+          icon: 'nextjs',
+          label: 'NextJs',
+        },
+        {
+          icon: 'angular',
+          label: 'Angular',
+        },
+        {
+          icon: 'dotnet',
+          label: 'ASPNET Core Web API',
+        },
+        {
+          icon: 'tailwindcss',
+          label: 'Tailwindcss',
+        },
+        {
+          icon: 'angular',
+          label: 'Angular Material',
+        },
+      ],
+    },
+    {
+      companyName: 'DZentra Pvt Ltd',
+      responsibility:
+        'Create a monitoring tool dashboard, Create charts and visual graphics for the dashboard.',
+      designation: 'ReactJs Developer - Internship',
+      techStack: [
+        {
+          icon: 'reactjs',
+          label: 'ReactJs',
+        },
+        {
+          icon: 'bootstrap',
+          label: 'Bootstrap',
+        },
+      ],
+    },
+    {
       companyName: 'Koshur Store',
       responsibility:
         'Creating the pages in ReactJs for the store app and handling the backend in firebase. Whole application is based on online store to deliver the items to customer.',
       designation: 'Junior ReactJs Developer - Internship',
-    },
-    {
-      companyName: 'DZentra Pvt Ltd',
-      responsibility: '',
-      designation: 'ReactJs Developer - Internship',
-    },
-    {
-      companyName: 'de facto Infotech',
-      responsibility: '',
-      designation: 'Fullstack developer - Fulltime',
+      techStack: [
+        {
+          icon: 'reactjs',
+          label: 'ReactJs',
+        },
+        {
+          icon: 'firebase',
+          label: 'Firebase',
+        },
+        {
+          icon: 'bootstrap',
+          label: 'Bootstrap',
+        },
+      ],
     },
   ];
 }
--- src/app/core/services/responsive.service.ts
+++ src/app/core/services/responsive.service.ts
@@ -10,7 +10,7 @@ export class ResponsiveService {
 
   private screenwidth = toSignal(
     this.breakpointObserver.observe([
-      Breakpoints.XLarge,
+      Breakpoints.Large,
       Breakpoints.Medium,
       Breakpoints.Small,
     ])
@@ -18,5 +18,5 @@ export class ResponsiveService {
 
   $small = computed(() => this.screenwidth()?.breakpoints[Breakpoints.XSmall]);
   $medium = computed(() => this.screenwidth()?.breakpoints[Breakpoints.Medium]);
-  $large = computed(() => this.screenwidth()?.breakpoints[Breakpoints.XLarge]);
+  $large = computed(() => this.screenwidth()?.breakpoints[Breakpoints.Large]);
 }
