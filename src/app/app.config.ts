fe4adc510cffefc4c30134922de9a7d57fd93a71
AyushKumar2395 <ayush@ctsglobalconnect.com>
2025-06-05 16:07:20 +0530

    chore: upgrade Angular and related dependencies to version 20
    - Updated Angular packages to version 20.x.x in package.json.
    - Removed unused Firebase Auth, Database, and Storage imports in app.config.ts.
    - Cleaned up styles.scss by removing commented theming instructions and added base styles for html and body.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,7 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { provideStorage, getStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,10 +18,7 @@ export const appConfig: ApplicationConfig = {
       useValue: { appearance: 'outline' },
     },
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideDatabase(() => getDatabase()),
-    provideStorage(() => getStorage()),
   ],
 };
 
